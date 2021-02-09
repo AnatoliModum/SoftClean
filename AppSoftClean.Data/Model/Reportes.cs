25703f9099c52801f4fcd3a60a8b7b40f47f7949
Alejandro Olvera <AnatoliModum@gmail.com>
2021-02-09 16:49:25 -0600

    Creacion de entidad de reportes
    Se creo la entidad de reportes y un metodo para el fill de sus datos en formato string

---
+++ AppSoftClean.Data/Model/Reportes.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppSoftClean.Data.Model
+{
+    class Reportes
+    {
+        public string AreaInstalacion { get; set; } 
+        public string ModeloEquipoDosificador { get; set; } //
+        public string DosificadorEstacionDeLimpieza { get; set; } //
+        public List<String> ProductosQuimicos { get; set; }
+        public string ModeloJabonera { get; set; } //
+        public int CantidadConsumibles { get; set; }
+        public string TipoMaquinaLavavajillas { get; set; } //
+        public List<String> DosificadoresLavavajillas { get; set; }
+        public string PortaGalones { get; set; } //
+        public string Division { get; set; }
+        public string Fecha { get; set; }
+        public int NumeroDeHoja { get; set; }
+    }
+}
